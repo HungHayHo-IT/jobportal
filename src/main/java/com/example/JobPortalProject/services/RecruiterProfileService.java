c52f4142c1b8dc19cd41bc501e6b1044cabef924
HungHayHo <nguyenphuocnhahungl97@gmail.com>
2026-01-31 16:09:03 +0700

    controller and service

---
+++ src/main/java/com/example/JobPortalProject/services/RecruiterProfileService.java
@@ -0,0 +1,22 @@
+package com.example.JobPortalProject.services;
+
+import com.example.JobPortalProject.entity.RecruiterProfile;
+import com.example.JobPortalProject.repository.RecruiterProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class RecruiterProfileService {
+    private final RecruiterProfileRepository recruiterProfileRepository;
+
+    @Autowired
+    public RecruiterProfileService(RecruiterProfileRepository recruiterProfileRepository) {
+        this.recruiterProfileRepository = recruiterProfileRepository;
+    }
+
+    public Optional<RecruiterProfile> getOne(Integer id ){
+        return recruiterProfileRepository.findById(id);
+    }
+}
